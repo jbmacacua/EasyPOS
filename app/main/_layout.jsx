ec8230bb8e79032d63930d33b9798c061ea0c994
jb <116430791+jbmacacua@users.noreply.github.com>
2024-12-11 16:30:49 +0800

    feat(nav-bar): add navigation bar

--- app/main/_layout.jsx
+++ app/main/_layout.jsx
@@ -1,15 +1,78 @@
-
-
 import { Tabs } from 'expo-router';
-import 'react-native-reanimated';
+import { MaterialIcons } from '@expo/vector-icons'; // Import icons for the tabs
+import { View } from 'react-native';
+import Octicons from '@expo/vector-icons/Octicons';
 
 export default function TabsLayout() {
   return (
-      <Tabs>
-        <Tabs.Screen name="dashboard" options={{ headerShown: false }} />
-        <Tabs.Screen name="inventory" options={{ headerShown: false}}/>
-        <Tabs.Screen name='sales' options={{ headerShown: false}}/>
-        <Tabs.Screen name='profile' options={{ headerShown: false}}/>
-      </Tabs>
+    <Tabs
+      screenOptions={({ route }) => ({
+        tabBarShowLabel: false,
+        tabBarStyle: { height: 65, backgroundColor: '#3C80B4', paddingTop: 12 },
+        tabBarActiveTintColor: '#3C80B4',
+        tabBarInactiveTintColor: '#FFFFFF',
+        tabBarIcon: ({ focused, color }) => {
+          let iconName;
+
+          switch (route.name) {
+            case 'dashboard':
+              iconName = 'home';
+              break;
+            case 'inventory':
+              return (
+                <View
+                  className={`w-12 h-12 rounded-full ${
+                    focused ? 'bg-white' : 'bg-transparent'
+                  } flex items-center justify-center`}
+                >
+                  <Octicons name="checklist" size={24} color={color} />
+                </View>
+              );
+            case 'sales':
+              iconName = 'shopping-cart';
+              break;
+            case 'profile':
+              iconName = 'person';
+              break;
+            default:
+              iconName = 'circle';
+          }
+          return (
+            <View
+              className={`w-12 h-12 rounded-full ${
+                focused ? 'bg-white' : 'bg-transparent'
+              } flex items-center justify-center`}
+            >
+              <MaterialIcons name={iconName} size={28} color={color} />
+            </View>
+          );
+        },
+      })}
+    >
+      <Tabs.Screen
+        name="dashboard"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Tabs.Screen
+        name="sales"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Tabs.Screen
+        name="inventory"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Tabs>
   );
 }
