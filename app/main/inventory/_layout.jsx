e4ea374347af40e52e9e66c5b1a9b2eff55c3a42
jbmacacua <116430791+jbmacacua@users.noreply.github.com>
2025-02-01 21:31:37 +0800

    refactor: change folder structure

---
+++ app/main/inventory/_layout.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Tabs } from "expo-router"
+
+const InventoryLayout = () => {
+    return <Tabs screenOptions={{ headerShown: false, tabBarStyle: { display: "none" } }} />
+}
+
+export default InventoryLayout
