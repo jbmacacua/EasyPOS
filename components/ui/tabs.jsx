e010ced874a668ed80c0ef0e26d74f692182278d
Cris Julyan C. Olesco <92448053+crisjulyancolesco@users.noreply.github.com>
2025-04-24 21:18:55 +0800

    style(ui-tabs): responsive tab width base on device dimension

--- components/ui/tabs.jsx
+++ components/ui/tabs.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-import { TouchableOpacity, Text, View } from "react-native";
+import { TouchableOpacity, Text, View, Dimensions } from "react-native";
 import { useSession } from "@context/auth"; // <-- useSession instead of useAuth
 
 const Tabs = ({ activeTab, setActiveTab }) => {
     const { userRole } = useSession(); // <-- useSession hook here
+    const screenWidth = Dimensions.get('window').width;
+    const tabWidth = screenWidth / 3; // Adjust the width of each tab based on screen size
 
   const tabs = userRole === 'sales' ? ['Daily'] : ['Daily', 'Weekly', 'Monthly'];
 
@@ -12,7 +14,10 @@ const Tabs = ({ activeTab, setActiveTab }) => {
       {tabs.map((tab) => (
         <TouchableOpacity
           key={tab}
-          className={`px-5 py-2 w-[90px] rounded-full mx-2 shadow-md ${activeTab === tab ? "bg-[#3C80B4]" : "bg-white border border-[#1C547E]"}`}
+          style={{
+            width: tabWidth - (tabWidth * 0.3),
+          }}
+          className={`px-5 py-2 rounded-full mx-2 shadow-md ${activeTab === tab ? "bg-[#3C80B4]" : "bg-white border border-[#1C547E]"}`}
           onPress={() => setActiveTab(tab)}
         >
           <Text className={`font-bold text-center ${activeTab === tab ? "text-white" : "text-black"}`}>
