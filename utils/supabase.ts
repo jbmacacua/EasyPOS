6910f74b25b80915ba922465afc00d5896e19c00
jbmacacua <116430791+jbmacacua@users.noreply.github.com>
2025-04-26 22:22:03 +0800

    chore(env): add environment variables for Supabase config

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,10 +1,14 @@
 // Base
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { createClient } from "@supabase/supabase-js"
+import Constants from "expo-constants";
+
+const supabaseUrl = Constants.expoConfig?.extra?.EXPO_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = Constants.expoConfig?.extra?.EXPO_PUBLIC_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(
-  "https://uvhbiqrgjfklphcwcxqa.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV2aGJpcXJnamZrbHBoY3djeHFhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI5NDU4NTAsImV4cCI6MjA0ODUyMTg1MH0.Upidie78d18cn1iX5LdIvaiKM7kKB18nxLsgUKkMlrg",
+  supabaseUrl,
+  supabaseAnonKey,
   {
     auth: {
       storage: AsyncStorage,
